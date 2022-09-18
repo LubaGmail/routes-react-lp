bf5fd5bc2c22cac5c2e86898bbc01949eb8930b4
LubaGmail <Powe2009@gmail.com>
2022-09-18 11:42:23 -0400

    1 file modified

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -4,8 +4,15 @@ import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 const NewMeetup = (props) => {
   const addMeetup = (meetupRecord) => {
-    console.log('record', meetupRecord);
+    fetch('https://meetups-7d92d-default-rtdb.firebaseio.com/meetups.json', {
+      method: 'POST',
+      body: JSON.stringify(meetupRecord),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
   };
+
   return (
     <div>
       <NewMeetupForm onAddMeetup={addMeetup} />
