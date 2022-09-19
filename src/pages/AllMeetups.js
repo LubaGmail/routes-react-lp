58d699ea7fda2d7b2bcc5568fc2159a46b6dfb74
LubaGmail <Powe2009@gmail.com>
2022-09-19 16:12:05 -0400

    1 file modified

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import DUMMY_DATA from '../data/meetups';
 import MeetupList from '../components/meetups/MeetupList';
 
 const AllMeetups = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch('https://meetups-7d92d-default-rtdb.firebaseio.com/meetups.json')
+      .then((res) => res.json())
+      .then((data) => {
+        const arr = [];
+        for (let key in data) {
+          let meetup = {
+            id: key,
+            ...data[key],
+          };
+          arr.push(meetup);
+        }
+        setData(arr);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log('AllMeetups.fetch error', error);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <section>Loading...</section>;
+  }
+
   return (
-    <div>
+    <section>
       <h3>All Meetups</h3>
-      <MeetupList data = {DUMMY_DATA} />
-    </div>
+      <MeetupList data={data} />
+    </section>
   );
 };
 
